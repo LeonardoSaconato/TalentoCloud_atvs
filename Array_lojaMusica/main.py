3b422d9a417515b325460aa2ef76cfba080d0538
LeonardoSaconato <leonardo.saconato@hotmail.com>
2023-10-09 07:28:21 -0300

    Arrays_exe

---
+++ Array_lojaMusica/main.py
@@ -0,0 +1,12 @@
+## Nome: Leonardo Saconato de Santana
+## Turma 08 
+
+# Array de produtos de uma loja de música
+loja_musica = ["Violão", "Guitarra", "Microfone", "Cavaquinho", "CD'S","LP's"]
+
+# Array de anos de nascimento de familiares e amigos
+anos_nascimento = [1971, 1974,1992, 2003, 2006]
+
+# Imprimindo os arrays no terminal
+print("Produtos de uma loja de música:\n", loja_musica)
+print("Anos de nascimento de familiares e amigos:\n", anos_nascimento)
